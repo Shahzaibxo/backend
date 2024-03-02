53845ebcc3aad76cf377dadda2e4e97c22809dab
Shahzaibxo <shahziwork@gmail.com>
2024-03-02 18:21:11 +0500

    backend fix

--- script.py
+++ script.py
@@ -11,7 +11,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/", methods=['POST'])
+@app.route("/")
+def basic():
+    return 'Coding ki MKC!!'
+
+@app.route("/email", methods=['POST'])
 
 def scrape_emails():
     data = request.json.get('data')
